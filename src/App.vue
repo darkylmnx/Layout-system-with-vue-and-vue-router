f6c3508c7ed881f21e114ba0796016fdc0e5ba89
otakodes <hello@otakod.es>
2018-06-01 16:08:23 +0200

    :construction: showing router bug

--- src/App.vue
+++ src/App.vue
@@ -14,6 +14,16 @@ export default {
     layout() {
       return (this.$route.meta.layout || default_layout) + "-layout";
     }
+  },
+
+  created() {
+    // nothing defined here (when this.$route.path is other than "/")
+    console.log(this.$route, this.$route.meta.layout);
+  },
+
+  updated() {
+    // something defined here whatever the this.$route.path
+    console.log(this.$route, this.$route.meta.layout);
   }
 };
 </script>
